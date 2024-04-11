c459febc51c99872f81f3105a3ddeab017660ba3
Artem Lobzov <psychodiss@gmail.com>
2024-04-11 17:12:48 +0300

    show working proxy form the list

--- test.py
+++ test.py
@@ -1,11 +1,23 @@
-# вводные данные
-number = int(input())
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
-# продолжите решение здесь
-first_digit = 0
-while number > 0:
-    last_digit = number % 10
-    first_digit = number
-    number //= 10
+proxy_list = ['8.210.83.33:80', '199.60.103.28:80',
+              '103.151.246.38:10001', '199.60.103.228:80',
+              '199.60.103.228:80', '199.60.103.28:80', ]
 
-print(first_digit)
+for PROXY in proxy_list:
+    try:
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument('--proxy-server=%s' % PROXY)
+        url = 'https://2ip.ru/'
+
+        with webdriver.Chrome(options=chrome_options) as browser:
+            browser.get(url)
+            print(browser.find_element(By.ID, 'd_clip_button').find_element(By.TAG_NAME, 'span').text)
+
+            browser.set_page_load_timeout(5)
+
+            proxy_list.remove(PROXY)
+    except Exception as _ex:
+        print(f"Превышен timeout ожидания для - {PROXY}")
+        continue
